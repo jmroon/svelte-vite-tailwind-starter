5b97b38f5adbdd4ea5c22a6ce1f4d8acde099b8f
jmroon <36086006+jmroon@users.noreply.github.com>
2022-01-04 14:16:07 -0500

    initial commit

---
+++ src/lib/Counter.svelte
@@ -0,0 +1,14 @@
+<script>
+  let count = 0
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<button class="bg-red-200 w-96 h-24 rounded-full" on:click={increment}>
+  Clicks: {count}
+</button>
+
+<style>
+ 
+</style>
